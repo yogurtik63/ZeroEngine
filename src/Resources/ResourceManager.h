fa151a923ae4558769bac94218b8758b85f45632
A. Bondarenko <reidsanya@yandex.ru>
2023-02-01 21:54:25 +0300

    Added Texturing

--- src/Resources/ResourceManager.h
+++ src/Resources/ResourceManager.h
@@ -6,6 +6,7 @@
 
 namespace Renderer {
 	class ShaderProgram;
+	class Texture2D;
 }
 
 class ResourceManager {
@@ -21,11 +22,17 @@ public:
 	std::shared_ptr<Renderer::ShaderProgram> loadShaders(const std::string& shaderName, const std::string& vertexPath, const std::string& fragmentPath);
 	std::shared_ptr<Renderer::ShaderProgram> getShaderProgram(const std::string& shaderName);
 
+	std::shared_ptr<Renderer::Texture2D> loadTexture(const std::string& textureName, const std::string& texturePath);
+	std::shared_ptr<Renderer::Texture2D> getTexture(const std::string& textureName);
+
 private:
 	std::string getFileString(const std::string& relativeFilePath) const;
 
 	typedef std::map<const std::string, std::shared_ptr<Renderer::ShaderProgram>> ShaderProgramsMap;
 	ShaderProgramsMap m_shaderPrograms;
+
+	typedef std::map<const std::string, std::shared_ptr<Renderer::Texture2D>> TexturesMap;
+	TexturesMap m_textures;
 
 	std::string m_path;
 };
